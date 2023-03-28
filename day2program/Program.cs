3eebb50dadd60f8fe0cbc68a95812393eaf85d8f
Admin <Admin@ITACHI>
2023-03-28 09:10:11 +0545

    Add project files.

---
+++ day2program/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day2program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            person Melina = new person();
+         //   Melina.numberofeyes = 2;
+            Melina.noofeyes = 3;
+            Melina.haircolor = "black";
+            Console.WriteLine("You have" + Melina.noofeyes + "eyes");
+          //  Console.WriteLine("you have"+ Melina.numberofeyes+"eyes");
+            Console.WriteLine("Your hair color is " + Melina.haircolor);
+            //calling function from person.cs program
+            Melina.EatingHabit();
+            Melina.EatingHabit("Burger");
+            string muLunch = Melina.EatHabit("Rice");
+            Console.WriteLine(muLunch);
+            Console.ReadKey();
+          //  LogicalOperator();
+          //  Console.ReadKey();
+        }
+        //This function is example of logical operator
+        //static void LogicalOperator()
+        //{
+        //    Console.WriteLine("Enter your college name:");
+        //    string CollegeName=Console.ReadLine();    
+        //    Console.WriteLine("Enter your program");
+        //    string programName=Console.ReadLine();
+        //    if(string.IsNullOrEmpty(CollegeName))
+        //    {
+        //        Console.WriteLine("Please enter college name");
+        //    }
+        //    else if(CollegeName.ToUpper()=="BMC"&&programName.ToUpper()=="CSIT")
+        //    {
+        //        Console.WriteLine("Your are welcome in lab");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("You may not enter.");
+        //    }
+
+        //}
+    }
+}
---
+++ day2program/person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace day2program
+{
+    internal class person
+    {
+        int numberofeyes;
+        public int noofeyes
+        {
+            get { return numberofeyes; }
+            set { numberofeyes = value; }
+        }
+        public string haircolor
+        {
+            get; set;
+        }
+        public void EatingHabit()
+        {
+            Console.WriteLine("I usually have momo in lunch.");
+        }
+        public void EatingHabit(string myLunch )
+        {
+            Console.WriteLine("I usually have " + myLunch + " in lunch.");
+        }
+        public string EatHabit(string myLunch)
+        {
+            return "I usually have " + myLunch + " in my lunch";
+        }
+    }
+}
